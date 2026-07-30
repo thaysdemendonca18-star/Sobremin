c2d184bfebfa3ba8be5c9fa445577b75ba0f6c0b
Thays de Mendonca <thaysdemendonca.18@gmail.com>
2026-07-30 11:55:42 -0300

    atualização

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 function modoEscuro() {
         document.body.classList.toggle("dark");
 
-        const botao = document.querySelector("button");
+        const botao = document.getElementById("botaotema");
 
         if (document.body.classList.contains("dark")) {
           botao.innerHTML = "☀️ Modo Claro";
